8c03f20eda334199e4d7d174c64d9237690f7df3
angelozehr <angelo.zehr@gmx.ch>
2015-08-27 16:24:49 +0200

    added text in header and fixed height of articles

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('This would be the main JS file.');
+jQuery(document).ready(function($) {
+	$("#main_content").each(function(event) {
+		var titleHeight = 0 ;
+		var textHeight  = 0 ;
+		$(".project").each(function(event) {
+			if( $(this).find("h3").height() > titleHeight ) titleHeight = $(this).find("h3").height();
+			if( $(this).find("p").height() > textHeight ) textHeight = $(this).find("p").height();
+		});
+		$(".project h3").height(titleHeight);
+		$(".project p").height(textHeight);
+	});
+});
