a5617cf9dc3ac1df0f0978496d55c29da6ad5985
angelozehr <angelo.zehr@gmx.ch>
2015-08-28 11:25:13 +0200

    Added Floating workaround. For Real this time.

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,22 +2,26 @@ jQuery(document).ready(function($) {
 	adaptHeight();
 });
 
-$(window).resize(function() {
-	adaptHeight();
-});
+	$(window).on('resize', function(){
+		adaptHeight();
+	});
 
 function adaptHeight() {
-	$("#main_content").each(function(event) {
-		var titleHeight = 0 ;
-		var textHeight  = 0 ;
-		var totalHeight  = 0 ;
-		$(".project").each(function(event) {
-			if( $(this).find("h3").height() > titleHeight ) titleHeight = $(this).find("h3").height();
-			if( $(this).find("p").height() > textHeight ) textHeight = $(this).find("p").height();
-		});
-		$(".project h3").height(titleHeight);
-		$(".project p").height(textHeight);
-		totalHeight = $(".project").first().height();
-		$(".project:not(:eq(0))").height(totalHeight+1);
+	$("#main_content .project").height('auto');
+	$("#main_content .project h3").height('auto');
+	$("#main_content .project p").height('auto');
+	var titleHeight = 0 ;
+	var textHeight  = 0 ;
+	var totalHeight = 0 ;
+	$("#main_content .project").each(function(event) {
+		if( $(this).find("h3").height() > titleHeight ) titleHeight = $(this).find("h3").height();
+		if( $(this).find("p").height() > textHeight ) textHeight = $(this).find("p").height();
 	});
+	console.log(titleHeight);
+	$("#main_content .project h3").height(titleHeight);
+	$("#main_content .project p").height(textHeight);
+	totalHeight = $("#main_content .project").first().height();
+	$("#main_content .project:not(:eq(0))").height(totalHeight+1);
+
+	console.log('titleHeight: '+titleHeight+' textHeight:'+textHeight+' totalHeight:'+totalHeight);
 }
