0968618520f14cc6b660045c487b8f2571de3ca3
angelozehr <angelo.zehr@gmx.ch>
2015-08-28 10:59:29 +0200

    Added Floating workaround

--- javascripts/main.js
+++ javascripts/main.js
@@ -10,11 +10,14 @@ function adaptHeight() {
 	$("#main_content").each(function(event) {
 		var titleHeight = 0 ;
 		var textHeight  = 0 ;
+		var totalHeight  = 0 ;
 		$(".project").each(function(event) {
 			if( $(this).find("h3").height() > titleHeight ) titleHeight = $(this).find("h3").height();
 			if( $(this).find("p").height() > textHeight ) textHeight = $(this).find("p").height();
 		});
 		$(".project h3").height(titleHeight);
 		$(".project p").height(textHeight);
+		totalHeight = $(".project").first().height();
+		$(".project:not(:eq(0))").height(totalHeight+1);
 	});
 }
